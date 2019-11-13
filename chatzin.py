f714ccb69b47973dd8a1ed455a37f420961d045c
Willian Moura <30304867+mrlactobacilo@users.noreply.github.com>
2019-11-13 13:57:47 -0400

    New chatzin
    A new thought about the chatzin

--- chatzin.py
+++ chatzin.py
@@ -1,64 +1,81 @@
 import socket
-import select
 import threading
 import sys
-
-
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
-
-
-def client_thread(conn, addr):
-    while True:
-        try:
-            sockets_list = [sys.stdin, conn]
-            read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
-            for socks in read_sockets:
-                if socks == conn:
-                    message = conn.recv(2048)
-                    print('<' + addr + '>' + message)
-                else:
-                    message = sys.stdin.readline()
-                    conn.send(message)
-                    sys.stdout.write("<You>")
-                    sys.stdout.write(message)
-                    sys.stdout.flush()
-        except:
-            print("Error")
-            continue
-
+import select
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+"""
+the first argument AF_INET is the address domain of the socket. This is used when we have an Internet Domain
+with any two hosts
+The second argument is the type of socket. SOCK_STREAM means that data or characters are read in a continuous flow
+"""
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.setblocking(10)
+if len(sys.argv) != 3:
+    print("Correct usage: script, IP address, port number")
+    exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+server.bind((IP_address, Port))
+# binds the server to an entered IP address and at the specified port number.
+# The client must be aware of these parameters
+server.listen(100)
+# listens for 100 active connections. This number can be increased as per convenience
+list_of_clients=[]
 
-print('Digite o endereço IP da máquina que deseja se conectar:\n')
-ip = str(sys.stdin.readline())
 
-print('Digite a porta desejada:\n')
-port = int(sys.stdin.readline())
-
-err = server.connect_ex((ip, port))
-
-if err == 0:
-    print('Conexão bem sucedida...')
-    threading.Thread(client_thread(server,ip))
-    server.close()
-else:
-    print(get_ip_address())
-    server.bind((get_ip_address(), port))
-    server.listen(1)
-
+def clientthread(conn, addr):
+    conn.send("Welcome to this chatroom!")
+    # sends a message to the client whose user object is conn
     while True:
-        print('Esperando conexão...')
+            try:
+                message = conn.recv(2048)
+                if message:
+                    print("<" + addr[0] + "> " + message)
+                    message_to_send = "<" + addr[0] + "> " + message
+                    broadcast(message_to_send, conn)
+                    # prints the message and address of the user who just sent the message on the server terminal
+                else:
+                    remove(conn)
+            except Exception as e:
+                print(e)
+                break
+            try:
+                if select.select([sys.stdin,], [], [])[0]:
+                    entrada = sys.stdin.readline()
+                    conn.send(entrada)
+                    sys.stdout.flush()
+            except Exception as e:
+                print(e)
+                break
 
-        conn, addr = server.accept()
 
-        print(addr[0] + " se conectou à sua máquina")
+def broadcast(message, connection):
+    for clients in list_of_clients:
+        if clients != connection:
+            try:
+                clients.send(message)
+            except:
+                clients.close()
+                remove(clients)
 
-        threading.Thread(client_thread(conn, addr))
 
-    conn.close()
-    server.close()
+def remove(connection):
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
+
+
+while True:
+    conn, addr = server.accept()
+    """
+    Accepts a connection request and stores two parameters, conn which is a socket object for that user, and addr which contains
+    the IP address of the client that just connected
+    """
+    list_of_clients.append(conn)
+    print(addr[0] + " connected")
+    # maintains a list of clients for ease of broadcasting a message to all available people in the chatroom
+    # Prints the address of the person who just connected
+    threading.Thread(clientthread(conn,addr))
+    # creates and individual thread for every user that connects
+
+conn.close()
+server.close()
