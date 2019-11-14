15a2a499ad6570a3894710ac5d616de40c68a64e
mrlactobacilo <willian_98@outlook.com>
2019-11-14 13:55:54 -0400

    Att to python36 syntax

---
+++ chat_server27.py
@@ -0,0 +1,112 @@
+'''
+Server python
+Este codigo deve ser executado para iniciar o server, que ficara esperando por conexoes e retransmitira em
+broadcast todas as mensagens enviadas pelos usuarios conectados
+'''
+
+import socket
+import threading
+import sys
+
+'''
+Funcao resonsavel por retornar o IP da maquina que esta rodando o script
+'''
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+'''
+Funcao responsavel por cada conexao, imprimindo a mensagem na tela do server e retransmitindo 
+as mensagens recebidas 
+'''
+def clientthread(conn, addr):
+    # enviando uma mensagem de boas vindas ao usuario que se conectou
+    conn.send('Welcome to this chatroom!')
+
+    while True:
+            try:
+                # Fica "ouvindo" a conexao conn a espera de pacotes enviados por ela
+                message = conn.recv(2048)
+                if message:
+                    # Se um pacote for recebido, e a mensagem nao for nula, a mesma sera impressa na tela
+                    print("<" + addr[0] + "> " + message)
+
+                    # em seguida sera adicionado o endereco do usuario que a enviou
+                    message_to_send = "<" + addr[0] + "> " + message
+
+                    # e por fim a mesma e re-transmitida para todos os usuarios conectados no server
+                    broadcast(message_to_send, conn)
+                else:
+                    # Se a mensagem for nula, o que corresponde a uma conexao instavel, remove-se a conexao do server
+                    remove(conn)
+            except:
+                continue
+
+
+'''
+Funcao responsavel por transmitir em broadcast uma mensagem para todas as conexoes da lista de usuarios conectados
+exceto para o proprio usuario que a enviou
+'''
+def broadcast(message,connection):
+    for client in clients:
+        if client != connection:
+            try:
+                client.send(message)
+            except:
+                client.close()
+                remove(client)
+
+
+'''
+Funcao responsavel por remover uma conexao da lista de usuarios conectados
+'''
+def remove(connection):
+    if connection in clients:
+        clients.remove(connection)
+
+
+# criando um socket para a conexao
+# AF_INET : define a familia de IP que sera utilizada (AF_ de Address Family)
+# SOCK_STREAM : define o tipo de conexao do socket, sendo este utilizado para TCP e SOCK_DGRAM para uma conexao UDP
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Aqui definimos que o kernel deve reutilizar essa porta, mesmo que esteja no estado de TIME_WAIT.
+# Isso pode ser util ao reiniciarmos o server e o socket continuar ativo.
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+if len(sys.argv) != 3:
+    print("Por favor, digite apos o nome do script, o IP e a porta desejada, ambos separados por um espaco.")
+    exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+
+# Vinculando o socket server a porta especificada. O lado cliente deve estar ciente desses valores
+server.bind((IP_address, Port))
+
+# Permitindo ate 10 conexoes
+server.listen(100)
+
+clients=[]
+
+while True:
+    print("IP do server: " + IP_address)
+    print("Porta do server: " + str(Port))
+    print("Aguardando conexoes...")
+
+    # Aceitando uma conexao. São retornados 2 parametros:
+    # conn: objeto socket para a conexao do usuario que acabou de conectar
+    # addr: endereço IP do usuario que acabou de conectar
+    conn, addr = server.accept()
+
+    # Adicionando o socket a lista de usuarios conectados
+    clients.append(conn)
+
+    # Imprimindo na tela o endereço de quem conectou-se
+    print(addr[0] + " conectou-se ao server")
+
+    # Cria uma thread individual para cada novo usuario conectado
+    threading.Thread(clientthread(conn, addr))
+
+conn.close()
+server.close()
---
+++ client27.py
@@ -0,0 +1,48 @@
+'''
+Client python
+Este codigo deve ser iniciado para conectar-se ao server ja iniciado
+'''
+
+import socket
+import select
+import sys
+
+# criando um socket para a conexão
+# AF_INET : define a familia de IP que sera utilizada (AF_ de Address Family)
+# SOCK_STREAM : define o tipo de conexao do socket, sendo este utilizado para TCP e SOCK_DGRAM para uma conexao UDP
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if len(sys.argv) != 3:
+    print("Por favor, digite apos o nome do script o IP e a porta desejada, ambos separados por um espaco.")
+    exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+
+# Conectando-se ao server
+conn.connect((IP_address, Port))
+
+'''
+Fazendo uso da biblioteca select, monitoramos dois buffers de leitura: 
+    stdin: entrada padrão do sistema (teclado)
+    conn: socket usado para a conexão com o server
+    
+Quando um desses buffers fizer alguma leitura será "pego" pelo select, então comparamos
+o buffer para verificarmos se é o socket ou a entrada padrão (stdin): 
+    -> Se for o socket, então o "if" será verdadeiro, e imprimimos a mensagem recebida na tela do usuário.
+    -> Se for a entrada padrão, então "else" será verdadeiro, e enviaremos a mensagem digitada pelo usuário para o server,
+    além de imprimi-la na tela também
+'''
+while True:
+    sockets_list = [sys.stdin, conn]
+    read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+    for socks in read_sockets:
+        if socks == conn:
+            message = socks.recv(2048).decode(encoding='utf_8', errors='strict')
+            print(message)
+        else:
+            message = sys.stdin.readline()
+            conn.send(message.encode(encoding='utf_8', errors='strict'))
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            sys.stdout.flush()
+server.close()
